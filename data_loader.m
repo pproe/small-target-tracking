7d0aa44bd974a63e62f4817a9064595e1f858478
pproe <proe0907@gmail.com>
2022-05-09 17:01:18 +0800

    Complete loader functionality & update gitignore

--- data_loader.m
+++ data_loader.m
@@ -1,11 +1,11 @@
-classdef DataLoader
+classdef data_loader
 
   % Data Loader class that allows the parsing of the VISO data
 
   properties
-    folder
-    nameTemplate
-    frameRange
+    folder          % Folder path that contains VISO dataset
+    nameTemplate    % string to specify filename format (see sprintf())
+    frameRange      % 2-element array indicating start & end of frames
   end
 
   methods
@@ -17,18 +17,18 @@ classdef DataLoader
     %   nameTemplate  pattern for generating image file names
     %                 (e.g. "%06d.jpg" for the VISO dataset)
     %   frameRange    range of images as 2-element array (e.g. [start, end])
-    function obj = DataLoader(folder, nameTemplate, frameRange)
+    function obj = data_loader(folder, nameTemplate, frameRange)
 
       if ~exist(folder, 'dir')
         error('Folder does not exist.')
       end
 
-      folderContent = dir([folder '/img/*'])
+      folderContent = dir(strcat(folder,'/img/'));
 
       if ~exist('frameRange','var')
         % frameRange does not exist, so default to all frames
-        numImages = size(folderContent, 1)
-        obj.frameRange = [1 numImages]
+        numImages = size(folderContent, 1);
+        obj.frameRange = [1 numImages-2]; % Discount '.' and '..' directories
       else
         obj.frameRange = frameRange;
       end
@@ -37,5 +37,34 @@ classdef DataLoader
       obj.nameTemplate = nameTemplate;
       
     end
+
+    % Load Image        Returns image from source folder
+    %
+    %   imgNumber       The number in the sequence of images 
+    %                   (From start of frameRange as imgNumber = 1)
+    function r = loadImage(obj, imgNumber)
+
+        if(imgNumber < 1)
+            error('Image Number not in frame range: Too Low')
+        end
+
+        if(imgNumber > obj.frameRange(2)-obj.frameRange(1)+1)
+            error('Image Number not in frame range: Too High')
+        end
+    
+        imgNumber = imgNumber + obj.frameRange(1) - 1;
+        imgFileName = sprintf(obj.nameTemplate, imgNumber);
+        imgFileLocation = strcat(obj.folder, '/img/', imgFileName);
+
+        % Returns image
+        r = imread(imgFileLocation);
+    end
+
+    % Get Regions       Returns matrix of regions of object locations
+    function r = getRegions(obj)
+        dataFileLocation = strcat(obj.folder, '/gt/gt.txt');
+        r = readmatrix(dataFileLocation, Range='A:F');
+    end
+
   end
 end
---
+++ object_tracking.m
@@ -0,0 +1,4 @@
+% 1) Initial Detection of Candidate Small Objects
+
+% 2) Tracking of Objects
+
