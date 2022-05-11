67e53f7e4d746bc3d2fc7b078b07ab13811a29d3
E.Gu <104988850+eamongu@users.noreply.github.com>
2022-05-11 15:10:58 +0800

    Update DataLoader.m

--- DataLoader.m
+++ DataLoader.m
@@ -18,7 +18,9 @@ classdef DataLoader
     
     % Candidate match discrimination
     
-    
+    hblob_area
+    hblob_centroid
+    hblob_bbox
     end
     
     methods
@@ -73,7 +75,64 @@ classdef DataLoader
                 
             % Candidate match discrimination
             
+            hblob=vision.BlobAnalysis('MinimumBlobArea',4,'MaximumCount',200);
+            obj.hblob_area={};
+            obj.hblob_centroid={};
+            obj.hblob_bbox={};
+            for i=1:(obj.interval-2)
+                [area,centroid,bbox] = hblob(obj.and_output{i});
+                obj.hblob_area{i}=area;
+                obj.hblob_centroid{i}=int16(centroid);
+                obj.hblob_bbox{i}=bbox;
+            end
+      %      shape=insertShape(car05_7_11.image{2},'rectangle',bbox, 'Linewidth',10);
             
+            for a=1:(obj.interval-2)  % and_output image
+                for b=1:size(obj.hblob_centroid{a},1)  % clusters inside
+                    mask=zeros(11,11);
+                    for ii=1:11
+                        for jj=1:11
+                               if (obj.hblob_centroid{a}(b,2)+(ii-6))>=1 && (obj.hblob_centroid{a}(b,2)+(ii-6))<=size(obj.image{a+1},1) && (obj.hblob_centroid{a}(b,1)+(jj-6))>=1 && (obj.hblob_centroid{a}(b,1)+(jj-6))<=size(obj.image{a+1},2)
+                                  % x,y coordination is
+                                  % actually the opposite for some reason
+                                  % get the grayscale values within the window
+                   
+                                        mask(ii,jj)=obj.image{a+1}(obj.hblob_centroid{a}(b,2)+(ii-6), obj.hblob_centroid{a}(b,1)+(jj-6));
+                               end
+                        end
+                    end
+                    mask_mean=mean(mask,'all');
+                    mask_std= std(mask,1, 'all');
+                    th= norminv([0.005 0.995],mask_mean,mask_std); % need to have a look
+                    th=int8(th);
+                    
+                    mask_binary=zeros(11,11);
+                    for ii=1:11
+                        for jj=1:1
+                             if (obj.hblob_centroid{a}(b,2)+(ii-6))>=1 && (obj.hblob_centroid{a}(b,2)+(ii-6))<=size(obj.image{a+1},1) && (obj.hblob_centroid{a}(b,1)+(jj-6))>=1 && (obj.hblob_centroid{a}(b,1)+(jj-6))<=size(obj.image{a+1},2)
+                                 mask_binary(ii,jj)=obj.and_output{a}(obj.hblob_centroid{a}(b,2)+(ii-6), obj.hblob_centroid{a}(b,1)+(jj-6));
+                             end
+                        end
+                    end
+                    
+                    % grow the region of object clusters in and_put images
+                    % I have tested with examples, it seems the growing
+                    % region of one cluster is unexpectedly large, I suspect that is because the th region computed above is too wide
+                    % in my understanding the th region should be around [200 256]
+                    for ii=1:11
+                        for jj=1:11
+                            if (obj.hblob_centroid{a}(b,2)+(ii-6))>=1 && (obj.hblob_centroid{a}(b,2)+(ii-6))<=size(obj.image{a+1},1) && (obj.hblob_centroid{a}(b,1)+(jj-6))>=1 && (obj.hblob_centroid{a}(b,1)+(jj-6))<=size(obj.image{a+1},2)
+                                if mask(ii,jj)>= th(1,1) && mask(ii,jj)<=th(1,2) && mask_binary(ii,jj)~=1
+                                    obj.and_output{a}(obj.hblob_centroid{a}(b,2)+(ii-6), obj.hblob_centroid{a}(b,1)+(jj-6))= 1;
+                                elseif mask_binary(ii,jj)==1
+                                    obj.and_output{a}(obj.hblob_centroid{a}(b,2)+(ii-6), obj.hblob_centroid{a}(b,1)+(jj-6))= 1;
+                                end
+                            end
+                              
+                        end
+                    end
+                end 
+            end
         end     
     end
 end
